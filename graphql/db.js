9f049fa95292e2e23fb112db3925d70f9faf55c1
Bomi Choi <hibomi97@gmail.com>
2021-08-01 02:14:52 +0900

    mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,49 @@
-export const people = [
+let movies = [
     {
         id: "0",
-        name: "Bomi",
-        age: 25,
-        gender: "female"
+        name: "Star Wars",
+        score: 8
     },
     {
         id: "1",
-        name: "Nicolas",
-        age: 30,
-        gender: "male"
+        name: "Avengers - The new one",
+        score: 9
+    },
+    {
+        id: "2",
+        name: "The Godfather I",
+        score: 7
+    },
+    {
+        id: "3",
+        name: "Logan",
+        score: 6
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getByID = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
